124eab67f52c930e55de2d3f399799c4523393c5
Cash-coder <vadymkozak3@gmail.com>
2022-07-22 17:18:41 +0200

    added pictures

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-headings = ('Name', 'Role', 'Salary', 'checkbox')
+headings = ('Name', 'Role', 'Salary', 'checkbox', 'Picture')
 data = (
-    ('Rolf', 'SOftware Engineer !!', '42K'),
-    ('Kovak', 'Relaxer', '420K'),
-    ('Vako', 'Feeling good expert', '42M'),
+    ('Rolf', 'SOftware Engineer !!', '42K', 'http://1tfu7b1m4mm33k2ocvwvp010.wpengine.netdna-cdn.com/wp-content/uploads/2015/11/What-is-content-intelligence.jpg'),
+    ('Kovak', 'Relaxer', '420K', 'https://thelabrynthoflife.files.wordpress.com/2014/06/power-of-intelligence.jpg'),
+    ('Vako', 'Feeling good expert', '42M', 'https://ecvo8ubt6cq.exactdn.com/wp-content/uploads/2021/07/f97acb41-5694-4b4b-94b3-af590226f170.jpg'),
 )
 
 @app.route("/")
