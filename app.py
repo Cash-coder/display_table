69e0b7be547db619e3fa86ea59ecde676223cd8f
Cash-coder <vadymkozak3@gmail.com>
2022-07-22 17:20:02 +0200

    added pics and fixed bug

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 headings = ('Name', 'Role', 'Salary', 'checkbox', 'Picture')
 data = (
     ('Rolf', 'SOftware Engineer !!', '42K', 'http://1tfu7b1m4mm33k2ocvwvp010.wpengine.netdna-cdn.com/wp-content/uploads/2015/11/What-is-content-intelligence.jpg'),
-    ('Kovak', 'Relaxer', '420K', 'https://thelabrynthoflife.files.wordpress.com/2014/06/power-of-intelligence.jpg'),
-    ('Vako', 'Feeling good expert', '42M', 'https://ecvo8ubt6cq.exactdn.com/wp-content/uploads/2021/07/f97acb41-5694-4b4b-94b3-af590226f170.jpg'),
+    ('Bahamut', 'Relaxer', '420K', 'https://thelabrynthoflife.files.wordpress.com/2014/06/power-of-intelligence.jpg'),
+    ('Freediver', 'Feeling good expert', '42M', 'https://ecvo8ubt6cq.exactdn.com/wp-content/uploads/2021/07/f97acb41-5694-4b4b-94b3-af590226f170.jpg'),
 )
 
 @app.route("/")
